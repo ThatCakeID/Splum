2802dae4aed45caef9804f67dd4b0d23f5ed1694
TheClashFruit <clash.fruit123@gmail.com>
2022-07-15 13:14:34 +0200

    feat: new toolbar

--- app/src/main/java/com/thatcakeid/splum/MainActivity.kt
+++ app/src/main/java/com/thatcakeid/splum/MainActivity.kt
@@ -2,15 +2,18 @@ package com.thatcakeid.splum
 
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import org.mozilla.geckoview.GeckoRuntime
-import org.mozilla.geckoview.GeckoSession
+import androidx.core.content.ContextCompat
+import mozilla.components.browser.menu.BrowserMenuBuilder
+import mozilla.components.browser.menu.item.SimpleBrowserMenuItem
+import org.mozilla.geckoview.*
 import org.mozilla.geckoview.GeckoSession.ContentDelegate
 import org.mozilla.geckoview.GeckoSession.ProgressDelegate
-import org.mozilla.geckoview.GeckoSessionSettings
-import org.mozilla.geckoview.GeckoView
+import mozilla.components.browser.toolbar.BrowserToolbar
+import mozilla.components.concept.toolbar.Toolbar
+import mozilla.components.ui.tabcounter.TabCounter
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,13 +23,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val view = findViewById<GeckoView>(R.id.geckoview)
-        val editTextSearch = findViewById<EditText>(R.id.editTextTextPersonName)
+        val geckoView = findViewById<GeckoView>(R.id.geckoview)
+        val toolBar = findViewById<BrowserToolbar>(R.id.toolBar)
+
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        /*
+        val editTextSearch = findViewById<EditText>(R.id.editTextTextPersonName)
+        val tabIcon = findViewById<TabCounter>(R.id.tabIcon)
+        val moreIcon = findViewById<ImageView>(R.id.moreIcon)
+        */
+
         val session = GeckoSession()
 
         session.contentDelegate = object : ContentDelegate {}
         progressBar.visibility = View.GONE
+
+        toolBar.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
+
+        val shareItem = SimpleBrowserMenuItem("Share…") { /* Do nothing */ }
+        val homeScreenItem = SimpleBrowserMenuItem("Add to Home screen") { /* Do nothing */ }
+        val openItem = SimpleBrowserMenuItem("Open in…") { /* Do nothing */ }
+        val settingsItem = SimpleBrowserMenuItem("Settings") { /* Do nothing */ }
+
+        val items = listOf(shareItem, homeScreenItem, openItem, settingsItem)
+        toolBar.display.menuBuilder = BrowserMenuBuilder(items)
+
+        toolBar.url = "https://google.com/"
 
         if (sRuntime == null) {
             sRuntime = GeckoRuntime.create(this)
@@ -39,17 +62,34 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         session.open(sRuntime!!)
-        view.setSession(session)
+        geckoView.setSession(session)
         session.loadUri("https://google.com")
 
+        toolBar.setOnUrlCommitListener { url ->
+            session.loadUri(url)
+            toolBar.url = url
+
+            true
+        }
+
+        /*
         editTextSearch.setOnEditorActionListener { _, actionId, _ ->
             session.loadUri(editTextSearch.text.toString())
             true
         }
 
+        tabIcon.setOnClickListener { _ ->
+
+        }
+
+        moreIcon.setOnClickListener { _ ->
+
+        }
+        */
+
         session.progressDelegate = object : ProgressDelegate {
             override fun onPageStart(session: GeckoSession, url: String) {
-                editTextSearch.setText(url)
+                toolBar.url = url
 
                 progressBar.visibility = View.VISIBLE
                 progressBar.progress = 0
@@ -61,11 +101,17 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChange(session: GeckoSession, progress: Int) {
                 progressBar.progress = progress
             }
-        }
 
-        session.navigationDelegate = object : GeckoSession.NavigationDelegate {
-            override fun onLocationChange(session: GeckoSession, url: String?) {
-                editTextSearch.setText(url)
+            override fun onSecurityChange(
+                session: GeckoSession,
+                securityInfo: ProgressDelegate.SecurityInformation
+            ) {
+                if(securityInfo.isSecure)
+                    toolBar.siteSecure = Toolbar.SiteSecurity.SECURE
+                else
+                    toolBar.siteSecure = Toolbar.SiteSecurity.INSECURE
+
+                super.onSecurityChange(session, securityInfo)
             }
         }
     }
