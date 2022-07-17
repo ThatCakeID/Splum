71469125974334f70ea719bf7f6c1a48db335bea
TheClashFruit <clash.fruit123@gmail.com>
2022-07-17 12:52:05 +0200

    feat: add function to the licenses option

--- app/src/main/java/com/thatcakeid/splum/fragments/SettingsFragment.kt
+++ app/src/main/java/com/thatcakeid/splum/fragments/SettingsFragment.kt
@@ -1,12 +1,25 @@
 package com.thatcakeid.splum.fragments
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.thatcakeid.splum.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val pref = findPreference<Preference>("licensePref")
+        pref!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Licenses")
+                .setMessage(resources.getString(R.string.settings_licenses_full))
+                .setPositiveButton("Ok") { _, _ -> }
+                .show()
+
+            return@OnPreferenceClickListener true
+        }
     }
 }
